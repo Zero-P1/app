11e8d97744d4b6ac936293373f6f915248966df0
Zero-P1 <zeropontos.cnhdigital@gmail.com>
2024-09-04 00:08:14 +0000

    Removendo pasta app

--- app/src/assets/check.tsx
+++
@@ -1,5 +0,0 @@
-export function Check () {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7.5 8.75 1.667 1.667 3.75-3.75m3.75 10.833v-11c0-1.4 0-2.1-.273-2.635a2.5 2.5 0 0 0-1.092-1.093c-.535-.272-1.235-.272-2.635-.272H7.333c-1.4 0-2.1 0-2.635.272a2.5 2.5 0 0 0-1.092 1.093C3.333 4.4 3.333 5.1 3.333 6.5v11l2.292-1.667L7.708 17.5 10 15.833l2.292 1.667 2.083-1.667z"></path></svg>
-  )
-}
--- app/src/assets/person.tsx
+++
@@ -1,5 +0,0 @@
-export function Person() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16.667 17.5c0-1.163 0-1.745-.144-2.218a3.33 3.33 0 0 0-2.222-2.222c-.473-.143-1.055-.143-2.218-.143H7.917c-1.163 0-1.745 0-2.218.143a3.33 3.33 0 0 0-2.222 2.222c-.144.473-.144 1.055-.144 2.218M13.75 6.25a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0"></path></svg>
-  )
-}
